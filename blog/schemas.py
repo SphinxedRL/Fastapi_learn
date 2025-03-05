7f8a1b7775e782de867a5d47b6780cd9e99f0a8d
sphinxdot <sarahidikula@Sarahs-MacBook-Air.local>
2025-03-05 11:02:23 +0530

    Specify response model for individual 'get' command

--- blog/schemas.py
+++ blog/schemas.py
@@ -2,3 +2,8 @@ from pydantic import BaseModel
 class Blog(BaseModel):
     title: str
     body:str
+
+class Show_blog(BaseModel): #used to display response model as we want instead of all parameters in the schema stored in db
+    title:str
+    class Config:
+        from_attributes = True

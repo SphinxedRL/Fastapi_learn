123887525e140a83ddc46e3ed7c553d3563bd181
sphinxdot <sarahidikula@Sarahs-MacBook-Air.local>
2025-03-05 19:32:17 +0530

    Relational database(auto-assign all blogs to user1)

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,7 @@
 #table models
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from .database import Base
-class Blog(Base):
-    __tablename__= "blogs"
-
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String)
-    body = Column(String)
-
+from sqlalchemy.orm import relationship 
 class User(Base):
     __tablename__="Users"
 
@@ -15,5 +9,17 @@ class User(Base):
     name = Column(String)
     email = Column(String)
     password = Column(String)
+    blogs = relationship("Blog", back_populates="creator")
+
+class Blog(Base):
+    __tablename__= "blogs"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    body = Column(String)
+    user_id = Column(Integer, ForeignKey('Users.id'))
+    creator = relationship("User", back_populates="blogs")
+
+
 
 
--- blog/schemas.py
+++ blog/schemas.py
@@ -1,20 +1,30 @@
 from pydantic import BaseModel
+from typing import List
+
 class Blog(BaseModel):
     title: str
     body:str
-
-class Show_blog(BaseModel): #used to display response model as we want instead of all parameters in the schema stored in db
-    title:str
     class Config:
-        from_attributes = True
+         from_attributes = True
+
 
 class User(BaseModel):
     name: str
     email: str
     password: str
 
+
 class Show_user(BaseModel):
     name: str
     email: str
+    blogs: List[Blog] =[]
     class Config:
          from_attributes = True
+
+
+class Show_blog(BaseModel): #used to display response model as we want instead of all parameters in the schema stored in db
+    title:str
+    body: str
+    creator: Show_user
+    class Config:
+        from_attributes = True
